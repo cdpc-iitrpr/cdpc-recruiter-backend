652fd7e73c79f56a8968b8567b343de81b5baae0
Akshat Toolaj Sinha <2020csb1068@iitrpr.ac.in>
2023-08-11 22:56:16 +0530

    Added path in urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
-from .views import signup, signin, verify, logout
+from .views import *
 
 # Define all the URL patterns here
 urlpatterns = [
     path('signup/', signup),
     path('login/', signin),
     path('verify/', verify),
-    path('logout/', logout)
+    path('logout/', logout),
+    path('signout/', signout),
+    path('recruiterJAF',RecruiterJAF),
+    path('recruiterSubmitJAF/<int:form_id>',RecruiterSubmitJAF),
+    path('recruiterINF',RecruiterINF),
+    path('recruiterSubmitINF/<int:form_id>',RecruiterSubmitINF),
+    path('spocDetails/',SpocDetails),
+    path('departmentPrograms/<str:degree>',DepartmentPrograms),
 ]
