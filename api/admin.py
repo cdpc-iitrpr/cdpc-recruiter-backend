3c66209b4059d4971cb45310e01fb6e543d8c899
cdpc-iitrpr <129580940+cdpc-iitrpr@users.noreply.github.com>
2023-08-21 09:27:05 +0530

    Update admin.py
    Updated user model so that password hash form is present.

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,27 @@
 from django.contrib import admin
 from .models import *
+from django.contrib.auth.admin import UserAdmin
+
+
+class MyAdmin(UserAdmin):
+    list_display = ('username','email','is_staff','last_login')
+    search_fields = ('username','email')
+    readonly_fields = ('last_login','date_joined', 'id',)
+    filter_horizontal = ()
+    add_fieldsets = (
+            (
+                None,
+                {
+                    'classes': ('wide',),
+                    'fields': ('username', 'email', 'password1', 'password2'),
+                },
+            ),
+        )
+    list_filter= ()
+    fieldsets = ()
+
 # Register your models here.
-admin.site.register(User,admin.ModelAdmin)
+admin.site.register(User,MyAdmin)
 admin.site.register(JAFForm,admin.ModelAdmin)
 admin.site.register(INFForm,admin.ModelAdmin)
 admin.site.register(TestType,admin.ModelAdmin)
